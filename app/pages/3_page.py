abed6aaae86bbf4457f5b9990023620662333c82
PascalData <passi@bertramweb.de>
2025-03-13 08:41:53 +0100

    Zwischen-Commit
    - Einfach damit die blöden "changed files" in Github Desktop verschwinden xD

--- app/pages/3_page.py
+++ app/pages/3_page.py
@@ -1,21 +1,41 @@
+import matplotlib.pyplot as plt
 import pandas as pd
-import plotly.express as px
 import streamlit as st
-import time
 
-# Tab-Titel und -Icon festlegen
+# DataFrame laden:
+movies = pd.read_csv(r'../data/imdb_clean.csv')
+
+# Tab-Titel und -Icon festlegen:
 st.set_page_config(page_title='Finde Deine Filme', page_icon=':film_frames:')
 
-# Überschrift der Seite
-st.title('Beliebtheit von Filmen nach Regisseur / Schauspieler')
+# Überschrift der Seite:
+st.title('Bewertungsentwicklung von Filmen nach Regisseur & Schauspieler')
 
-# Caching für das Laden der Daten
-@st.cache_data
-def load_movies():
-    # Künstliche Verzögerung für den Ladeeffekt
-    time.sleep(5)
-    return pd.read_csv(r'../data/imdb_clean.csv')
+### REGISSEUR ###
+# Zentrierte Überschrift für die Regisseur-Analyse:
+st.header('Analyse nach Regisseur')
 
-# DataFrame auf Variable speichern
-movies = load_movies
+# Eingabefeld und Auswahl der Bewertungsart:
+with st.form('Auswahl_Regisseur'):
+    col1, col2 = st.columns(2)
 
+    # User-Input für den Namen des Regisseurs:
+    with col1:
+        name_director = st.text_input('Gib einen Regisseur-Namen ein:')
+        genres = st.checkbox('Wähle Deine Genres:', )
+
+    with col2:
+        rating_type_director = st.radio('Wähle den Bewertungstyp:',
+                                        ['Rating', 'Metascore'],
+                                        key='director')
+
+# Falls eine Eingabe durch den User getätigt wurde / vorhanden ist:
+if name_director:
+    # DataFrame nach Namen filtern und ggf. auf Variable speichern
+    movies_director = (movies[
+                       movies['Director']
+                       .str.contains(name_director, case=False, na=False)
+                       ])
+    # Prüfen, ob überhaupt Filme gefunden wurden
+    if not movies_director.empty:
+        movies_director = movies_director.sort_values(by='Year')
