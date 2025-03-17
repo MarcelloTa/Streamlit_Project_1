8f5b9521c3de82ef5c2825567c7c4cd6ada3df54
PascalData <passi@bertramweb.de>
2025-03-17 10:58:19 +0100

    Kleinstkorrekturen
    Punkte gem. Issue-Liste

--- app/Startseite.py
+++ app/Startseite.py
@@ -1,20 +1,35 @@
 import streamlit as st
 
-st.title("Startseite")
+# Tab-Titel und -Icon festlegen:
+st.set_page_config(page_title='FDF | Finde Deine Filme',
+                   page_icon=':film_frames:')
 
+# Überschrift setzen
+st.title("Finde Deine Filme :film_frames:")
 
-st.header("Gruppe 3 - Die Filmempfehlungs-App")
-st.write("Willkommen auf unserer Filmempfehlungs-App.")
-st.write("Hier kannst du dir anhand eines Genres die Top 3, 5 oder 10 "
-         "Filme eines bestimmten Jahres anzeigen lassen.\n Oder du möchtest mehr Infos über einen bestimmten Film haben?"
-         " Auch damit können wir dir helfen.\n Außerdem kannst du nach einem bestimmten Regisseur oder Schauspieler "
-         "suchen und dir seine besten Filme anzeigen lassen.")
+# Grußzeile
+st.write('Willkommen bei "Finde Deine Filme" - der App mit der Du '
+         'Deine Lieblingsfilme findest.')
 
-if st.button('Seite 1 - Filmübersicht'):
+# Infotext zur App
+st.write("Hier kannst Du Dir anhand eines Genres die Top 3, 5 oder 10 "
+         "Filme eines bestimmten Jahres anzeigen lassen (Filmübersicht). "
+         "Oder Du möchtest mehr Infos über einen bestimmten Film haben? "
+         "Auch damit können wir Dir helfen (Detaillierte Filmangaben). "
+         "Außerdem kannst Du nach einem bestimmten Regisseur oder Schauspieler "
+         "suchen und Dir die Bewertungen der entsprechenden Filme anzeigen "
+         "lassen (Filmstatistiken).")
+
+# Trennstreifen mit extra Abstand darunter zum Ausgleich
+st.divider()
+st.markdown('')
+
+# Link-Button zu den anderen Seiten
+if st.button('Seite 1 - Filmübersicht :star:'):
     st.switch_page("pages/1_Filmübersicht.py")
-if st.button('Seite 2 - Detailierte Filmangaben'):
-    st.switch_page("pages/2_Detailierte_Filmangaben.py")
-if st.button('Seite 3 - Filmstatistiken'):
+
+if st.button('Seite 2 - Detaillierte Filmangaben :mag_right:'):
+    st.switch_page("pages/2_Detaillierte_Filmangaben.py")
+
+if st.button('Seite 3 - Filmstatistiken :clapper:'):
     st.switch_page("pages/3_Filmstatistiken.py")
-
-
--- app/pages/1_Filmübersicht.py
+++ app/pages/1_Filmübersicht.py
@@ -6,28 +6,30 @@ import streamlit as st
 import time
 import datetime
 
-st.set_page_config(page_title='Die besten Filme')
+# Tab-Titel und -Icon festlegen:
+st.set_page_config(page_title='FDF | Finde Deine Filme', page_icon=':film_frames:')
 
+# customers = pd.read_csv(r'C:\Users\Admin\Documents\DataCraft\11_Datenvisualisierung_mit_Python\Streamlit_Project_1\data\imdb_clean.csv')
+customers = pd.read_csv(r'..\data\imdb_clean.csv')
 
-customers = pd.read_csv(r'C:\Users\Admin\Documents\DataCraft\11_Datenvisualisierung_mit_Python\Streamlit_Project_1\data\imdb_clean.csv')
 categorical_cols = customers["Genre"].unique()
 
 
-st.title("Die Top 3, 5 und 10 der Filme-App")
+st.title("Die TOP 3, 5 oder 10 Filme :star:")
 
 with st.form("Darstellungsform"):
     col1, col2, col3 = st.columns(3)
 
     with col1:
         # Selectbox erstellt einen Dropdown mit auswählbaren Werten:
-        cat_selection = st.selectbox('Welche Kategorie interessiert dich?',
+        cat_selection = st.selectbox('Welches Genre interessiert Dich?',
                                      categorical_cols)
     with col2:
-        target_selection = st.slider("Aus welchem Jahr möchtest du den Film ansehen?", min_value=1929, max_value=2025)
+        target_selection = st.slider("Aus welchem Jahr?", min_value=1929, max_value=2025)
     with col3:
-        top_n_movie = st.radio("Top 3, 5 und 10 der Filme.", options=[3,5,10])
+        top_n_movie = st.radio("Top 3, 5 oder 10 der Filme?", options=[3, 5, 10])
     # Übermittlungsknopf hinzufügen (ohne Knopf nicht sinnvoll):
-    submit = st.form_submit_button('Diagramm erstellen', type='primary')
+    submit = st.form_submit_button('Filme anzeigen', type='primary')
 
 if submit:
     if cat_selection and target_selection and top_n_movie:
