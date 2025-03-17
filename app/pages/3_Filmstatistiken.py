f54dbce5247b7b027b0271e932c3467afd6fb575
Marcello <tambasciam@gmail.com>
2025-03-17 13:39:47 +0100

    Dateipfad ergänzt

--- app/pages/3_Filmstatistiken.py
+++ app/pages/3_Filmstatistiken.py
@@ -3,8 +3,8 @@ import plotly.express as px
 import streamlit as st
 
 # DataFrame laden:
-# movies = pd.read_csv(r'C:\Users\Admin\Documents\DataCraft\11_Datenvisualisierung_mit_Python\Streamlit_Project_1\data\imdb_clean.csv')
-movies = pd.read_csv(r'..\data\imdb_clean.csv')
+movies = pd.read_csv(r'C:\Users\Admin\Documents\DataCraft\11_Datenvisualisierung_mit_Python\Streamlit_Project_1\data\imdb_clean.csv')
+
 
 # Tab-Titel und -Icon festlegen:
 st.set_page_config(page_title='FDF | Finde Deine Filme', page_icon=':film_frames:')
